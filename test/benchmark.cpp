d509af4442135ec30583015a9ca5f6bd8de5a414
ruihong123 <wang4996@purdue.edu>
2022-05-29 16:32:02 -0400

    change the page registeration.

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -247,7 +247,11 @@ void cal_latency() {
 }
 
 int main(int argc, char *argv[]) {
-
+    std::cout << "Using Boost "
+              << BOOST_VERSION / 100000     << "."  // major version
+              << BOOST_VERSION / 100 % 1000 << "."  // minor version
+              << BOOST_VERSION % 100                // patch level
+              << std::endl;
   parse_args(argc, argv);
 
   DSMConfig config;
