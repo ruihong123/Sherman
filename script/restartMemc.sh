9d2675d9a9880374ff77a936ba922af5c2ca9023
ruihong123 <wang4996@purdue.edu>
2022-05-29 23:27:51 -0400

    node count initialization

--- script/restartMemc.sh
+++ script/restartMemc.sh
@@ -11,5 +11,5 @@ port=$(awk 'NR==2{print}' ../memcached.conf)
 #sleep 1
 
 # init 
-echo -e "set serverNum 0 0 1\r\n0\r\nquit\r" | nc ${addr} ${port}
-echo -e "set clientNum 0 0 1\r\n0\r\nquit\r" | nc ${addr} ${port}
+echo -e "set ComputeNum 0 0 1\r\n0\r\nquit\r" | nc ${addr} ${port}
+echo -e "set MemoryNum 0 0 1\r\n0\r\nquit\r" | nc ${addr} ${port}
