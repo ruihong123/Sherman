01caaaafbf3133a1178b487b07a51d55b48eddca
ruihong123 <wang4996@purdue.edu>
2022-06-06 17:08:23 -0400

    bigdata table scan

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -36,7 +36,7 @@ bool table_scan = false;
 bool use_range_query = false;
 
 //uint64_t kKeySpace = 64 * define::MB;
-uint64_t kKeySpace = 50*1024*1024; // bigdata
+uint64_t kKeySpace = 100*1024*1024; // bigdata
 //uint64_t kKeySpace = 50*1024*1024; //cloudlab
 double kWarmRatio = 0.8;
 
