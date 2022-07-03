930f741f69d0b8da04b6ee75f2f46bfebacd7f68
ruihong123 <wang4996@purdue.edu>
2022-07-03 17:48:18 -0400

    add cloudlab installation script

--- script/cloudlab_install.sh
+++ script/cloudlab_install.sh
@@ -66,9 +66,9 @@ function run_bench() {
   do
     echo "Set up the ${compute_shard[n]}"
     ssh -o StrictHostKeyChecking=no ${compute_shard[n]}  "screen -d -m pwd && cd /users/Ruihong && sudo apt update -y && sudo apt install memcached libmemcached-dev -y && git clone https://github.com/google/cityhash && cd cityhash/ && ./configure && make all check CXXFLAGS="-g -O3" && sudo make install && cd .. && sudo apt install libboost-all-dev -y && git clone  $github_repo && cd Sherman/ && mkdir build &&  cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt-get install -y libnuma-dev  && make all -j 32 " &
-#    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git checkout $gitbranch && git pull && make db_bench -j 32 && make Server -j 32 && sudo apt install numactl -y" &
+#    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/Sherman/build && git checkout $gitbranch && git pull && make all -j 32 " &
     echo "Set up the ${memory_shard[n]}"
-#    ssh -o StrictHostKeyChecking=no ${memory_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git checkout $gitbranch && git pull && make db_bench -j 32 && make Server -j 32 && sudo apt install numactl -y" &
+#    ssh -o StrictHostKeyChecking=no ${memory_shard[n]} "screen -d -m pwd && cd /users/Ruihong/Sherman/build && git checkout $gitbranch && git pull && make all -j 32 " &
     ssh -o StrictHostKeyChecking=no ${memory_shard[n]}  "screen -d -m pwd && cd /users/Ruihong && sudo apt update -y && sudo apt install memcached libmemcached-dev -y && git clone https://github.com/google/cityhash && cd cityhash/ && ./configure && make all check CXXFLAGS="-g -O3" && sudo make install && cd .. && sudo apt install libboost-all-dev -y && git clone  $github_repo && cd Sherman/ && mkdir build &&  cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt-get install -y libnuma-dev  && make all -j 32 " &
     n=$((n+1))
     sleep 1
