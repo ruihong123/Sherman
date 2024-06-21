fa9c2f590c42ccff195b10e74d8d2ebb53f9ee3d
wang4996 <wang4996@purdue.edu>
2024-06-21 14:10:34 -0500

    50 M data

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -48,7 +48,7 @@ uint64_t kKeySpace = 50ull*1024ull*1024ull; //8 key 8 value
 //uint64_t kKeySpace = 50*1024*1024; //8 key 8 value
 #endif
 double kWarmRatio = 0.8;
-bool use_zipf = true;
+bool use_zipf = false;
 double zipfan =0.99;
 
 std::thread th[kMaxThread];
